0f49751b2d4badf6bb0b480e5f9ce3e4a08e93bc
VivaceJamiel <jimpoy2@gmail.com>
2022-01-27 17:59:46 -0600

    fix fade out

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var circles = [];
+var num = -1;
 var white = {r: 255, g: 255, b: 255};
 var backgroundColor = {r: 255, g: 255, b: 255};
 var clicked = false;
-var clickedCircle = {x: 0, y: 0, size: 0, c: {r: 0, g: 0, b: 0}, speed: 0};
+var fade = 255;
+var clickedCircle = {x: 0, y: 0, size: 0, c: {r: 0, g: 0, b: 0}, speed: 0, opacity: 0};
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     for (var i = 0; i < 50; i++) {
-        var nCircle = new Circle(random(windowWidth), random(windowHeight), {r: random(255), g: random(255), b: random(255)}, random(80)+20, random(1, 3));
+        var nCircle = new Circle(random(windowWidth), random(windowHeight), {r: random(255), g: random(255), b: random(255)}, random(80)+20, random(1, 3), 255);
         circles.push(nCircle);
     }
     noStroke();
@@ -16,23 +18,30 @@ function setup() {
 function draw() {
     background(backgroundColor.r, backgroundColor.g, backgroundColor.b);
 
-
     for (var i = 0; i < circles.length; i++) {
-        fill(circles[i].c.r, circles[i].c.g, circles[i].c.b);
+        fill(circles[i].c.r, circles[i].c.g, circles[i].c.b, circles[i].opacity);
         ellipse(circles[i].x, circles[i].y, circles[i].size);
         circles[i].move(circles[i].y, circles[i].speed);
     }
 
     if (clicked) {
-        fill(clickedCircle.c.r, clickedCircle.c.g, clickedCircle.c.b);
+        fill(circles[num].c.r, circles[num].c.g, circles[num].c.b, circles[num].opacity);
         circle(clickedCircle.x, clickedCircle.y, clickedCircle.size);
         if (clickedCircle.size < windowWidth*2) {
             clickedCircle.size += 30;
         } else {
-            clickedCircle.size = 0;
-            clicked = false;
-            backgroundColor = {r: clickedCircle.c.r, g: clickedCircle.c.g, b: clickedCircle.c.b};
-            background(backgroundColor.r, backgroundColor.g, backgroundColor.b);
+            console.log(circles[num].opacity);
+            if (circles[num].opacity > 0) {
+                circles[num].opacity -= 2;
+            } else {
+                circles.splice(i, 1);
+                var nCircle = new Circle(random(windowWidth), random(windowHeight), {r: random(255), g: random(255), b: random(255)}, random(80)+20, random(1, 3));
+                circles[i] = nCircle;    
+                clickedCircle.size = 0;
+                clicked = false;
+                backgroundColor = {r: clickedCircle.c.r, g: clickedCircle.c.g, b: clickedCircle.c.b};
+                background(backgroundColor.r, backgroundColor.g, backgroundColor.b);    
+            }
         }
     }
 }
@@ -43,13 +52,10 @@ function mousePressed() {
 
     for (var i = 0; i < circles.length; i++) {
         var d = dist(mX, mY, circles[i].x, circles[i].y);
-        circle(mouseX, mouseY, 100);
         if (d < circles[i].size / 2) {
+            num = i;
             clickedCircle = circles[i];
             clicked = true;
-            circles.splice(i, 1);
-            var nCircle = new Circle(random(windowWidth), random(windowHeight), {r: random(255), g: random(255), b: random(255)}, random(80)+20, random(1, 3));
-            circles[i] = nCircle;
         }
     }
 }
@@ -61,6 +67,7 @@ class Circle {
         this.c = c;
         this.size = size;
         this.speed = speed;
+        this.opacity = 255;
     }
 
     move() {
