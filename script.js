8e05023abbba5993404d72b2f91fa90cc71a65be
VivaceJamiel <jimpoy2@gmail.com>
2022-01-27 17:41:32 -0600

    circle expand and replace color background

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var circles = [];
-backgroundColor = {r: 255, g: 255, b: 255};
+var white = {r: 255, g: 255, b: 255};
+var backgroundColor = {r: 255, g: 255, b: 255};
+var clicked = false;
+var clickedCircle = {x: 0, y: 0, size: 0, c: {r: 0, g: 0, b: 0}, speed: 0};
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -19,6 +22,19 @@ function draw() {
         ellipse(circles[i].x, circles[i].y, circles[i].size);
         circles[i].move(circles[i].y, circles[i].speed);
     }
+
+    if (clicked) {
+        fill(clickedCircle.c.r, clickedCircle.c.g, clickedCircle.c.b);
+        circle(clickedCircle.x, clickedCircle.y, clickedCircle.size);
+        if (clickedCircle.size < windowWidth*2) {
+            clickedCircle.size += 30;
+        } else {
+            clickedCircle.size = 0;
+            clicked = false;
+            backgroundColor = {r: clickedCircle.c.r, g: clickedCircle.c.g, b: clickedCircle.c.b};
+            background(backgroundColor.r, backgroundColor.g, backgroundColor.b);
+        }
+    }
 }
 
 function mousePressed() {
@@ -29,7 +45,8 @@ function mousePressed() {
         var d = dist(mX, mY, circles[i].x, circles[i].y);
         circle(mouseX, mouseY, 100);
         if (d < circles[i].size / 2) {
-            backgroundColor = {r: circles[i].c.r, g: circles[i].c.g, b: circles[i].c.b};
+            clickedCircle = circles[i];
+            clicked = true;
             circles.splice(i, 1);
             var nCircle = new Circle(random(windowWidth), random(windowHeight), {r: random(255), g: random(255), b: random(255)}, random(80)+20, random(1, 3));
             circles[i] = nCircle;
@@ -51,5 +68,12 @@ class Circle {
         if (this.y > windowHeight) {
             this.y = 0;
         }
+    }
+
+    expand() {
+        while (this.size < windowWidth*2) {
+            this.size += this.speed;
+        }
+        this.size = 0;
     }
 }
